3e5efdff8a2c07653bcf1b2e0dc6a6818f1cf661
Edu <edugacv@gmail.com>
2021-01-06 19:12:42 +0100

    Input search box working

--- js/app.js
+++ js/app.js
@@ -8,13 +8,28 @@ function processFlags(result){
     let countries = document.querySelector('#countries')
     let countryArray = result
     countries.innerHTML = ''
+    let inputBox = document.querySelector('#search')
     
     //For each country of the array, we create a div with their respective information
     countryArray.forEach(country => {
         Dom.addCountry(country)
     })
 
+    inputBox.addEventListener('input', function(e){
+        let {value} = e.target
+        let names = document.querySelectorAll('.country-name')
 
+        names.forEach(name => {
+            if(name.innerText.toLowerCase().includes(value)){
+                name.parentElement.parentElement.style.display='block'
+            } else {
+                name.parentElement.parentElement.style.display='none'
+            }
+            if(!value){
+                name.parentElement.parentElement.style.display='block'
+            }
+        })
+    })
 }
 
 //Dark Mode toggler
